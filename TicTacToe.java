84d66ac134cd40ddfc5707db8f29952896a832e5
VEDANT JADHAV <vedant3582@gmail.com>
2025-09-07 21:36:52 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe extends JFrame implements ActionListener {
+    private final JButton[][] buttons = new JButton[3][3];
+    private boolean playerX = true;
+
+    public TicTacToe() {
+        setTitle("Tic Tac Toe");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        // Initialize buttons and add to the frame
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 60));
+                buttons[i][j].setFocusPainted(false);
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton buttonClicked = (JButton) e.getSource();
+
+        // Set text and disable button for current player
+        if (buttonClicked.getText().equals("")) {
+            buttonClicked.setText(playerX ? "X" : "O");
+            buttonClicked.setEnabled(false);
+
+            if (checkForWinner()) {
+                JOptionPane.showMessageDialog(this, "Player " + (playerX ? "X" : "O") + " wins!");
+                resetBoard();
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "It's a draw!");
+                resetBoard();
+            }
+
+            playerX = !playerX; // Toggle player
+        }
+    }
+
+    private boolean checkForWinner() {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                buttons[i][1].getText().equals(buttons[i][2].getText()) &&
+                !buttons[i][0].getText().equals("")) {
+                return true;
+            }
+
+            if (buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                buttons[1][i].getText().equals(buttons[2][i].getText()) &&
+                !buttons[0][i].getText().equals("")) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+            buttons[1][1].getText().equals(buttons[2][2].getText()) &&
+            !buttons[0][0].getText().equals("")) {
+            return true;
+        }
+
+        if (buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+            buttons[1][1].getText().equals(buttons[2][0].getText()) &&
+            !buttons[0][2].getText().equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+        playerX = true;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TicTacToe game = new TicTacToe();
+            game.setVisible(true);
+        });
+    }
+}
+                                
